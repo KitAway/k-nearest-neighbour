d38f4ce6a37e76f579fc12e129d6477dc3c1d0d1
Max @ work <>
2020-02-25 10:54:22 +0000

    answers

--- kNN.py
+++ kNN.py
@@ -1,15 +1,27 @@
 from points import Point
 import random as rd
 
+def distances(point, points: list):
+    pair = list()
+    for p in points:
+       pair.append((p, p.distance(point)))
+    return pair
+
+def minK(pairs, k:int):
+    sortD =sorted(pairs, key=lambda x: x[1])
+    k_pairs = sortD[:k]
+    return [p for p, v in k_pairs]
+
 def kNN(point, points: list, k: int):
-    k_points = list()
+    dis = distances(point, points)
+    k_points = minK(dis, k)
     return k_points
 
 
 def main(n, k):
 
-    n_points = [Point(rd.random(), rd.random()) for i in range(n)]
-    point = Point(rd.random(), rd.random())
+    n_points = [Point(i, i**1.4) for i in range(n)]
+    point = Point(2, 8)
     k_points = kNN(point, n_points, k)
     print("k nearest neighbours of %s are:"%point)
     for p in k_points:
--- points.py
+++ points.py
@@ -1,7 +1,11 @@
+import math
 class Point:
     def __init__(self, x = 0, y = 0):
         self.x = x
         self.y = y
+
+    def distance(self, p: Point):
+        return math.sqrt( (self.x - p.x) **2 + (self.y - p.y)**2)
 
     def __repr__(self):
         return "Point(x: %f, y: %f)"%(self.x, self.y)
